a0d3300d7cf006dc177d04cf9d1649697bafa156
hyenne <hyeinlee601@gmail.com>
2022-08-08 13:56:17 +0900

    express.js structure

---
+++ server/app.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const cors = require("cors")
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})

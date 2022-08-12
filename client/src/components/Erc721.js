208592f5d2a8838f713387cd55d5c923846e38f4
hyenne <hyeinlee601@gmail.com>
2022-08-12 09:16:03 +0900

    get token image from ipfs

--- client/src/components/Erc721.js
+++ client/src/components/Erc721.js
@@ -1,15 +1,30 @@
 import { Navigate, useParams, Link } from "react-router-dom";
 import { TokenDetail } from "../pages";
 import { useState, useEffect } from "react";
+import axios from 'axios';
 
 function Erc721({ erc721, navigate, idx }) {
+    let [tokenMetadata, setTokenMetadata] = useState();
+    const getTokenMetadata = () => {
+        axios.get(erc721.tokenURI)
+            .then(res => setTokenMetadata(res.data))
+    }
+    useEffect(()=>{
+        getTokenMetadata();
+    }, [tokenMetadata])
     return (
         <div className="erc721token">
             <Link to={`/explore/${idx}`}>
                 Name: <span className="name">{erc721.name}</span>(
                 <span className="symbol">{erc721.symbol}</span>)
                 <div className="nft">id: {erc721.tokenId}</div>
-                <img src={erc721.tokenURI} width={300} />
+                {tokenMetadata && tokenMetadata.image ?
+                    <img
+                        src={tokenMetadata.image.replace("ipfs://", "https://ipfs.io/ipfs/")}
+                        width={300}
+                    /> :
+                    <img src={erc721.tokenURI} width={300} />
+                }
             </Link>
         </div>
     );
